bb2c03decdfb8b801f5da725a391b194181db8da
paul <xvendo@gmail.com>
2016-11-11 00:37:07 +0100

    Changed to lib folder

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'xvendo:tagit',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Implements tagit JQUERY Plugin',
   // URL to the Git repository containing the source code for this package.
@@ -14,5 +14,5 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
   api.use('mizzao:jquery-ui@1.11.4');
-  api.addFiles(['src/js/tag-it.min.js', 'src/css/jquery.tagit.css'], 'client');  
+  api.addFiles(['lib/js/tag-it.min.js', 'lib/css/jquery.tagit.css'], 'client');  
 });
