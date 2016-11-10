c58959f86ee2be4fc3a531161dae1477d2bf98b2
paul <xvendo@gmail.com>
2016-11-10 03:40:02 +0100

    Package changed

--- package.js
+++ package.js
@@ -13,5 +13,6 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
+  api.use('mizzao:jquery-ui@1.11.4');
   api.addFiles(['src/js/tag-it.min.js', 'src/css/jquery.tagit.css'], 'client');  
 });
